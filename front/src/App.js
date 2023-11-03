74e0c49adac0ad002424eb7afb37d3a5631176b7
Giancarlo Tricerri <104391091+gtricerriv@users.noreply.github.com>
2023-11-03 12:52:47 -0400

    add MUI packages

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const data = [
+    { name: "John Doe", age: 30 },
+    { name: "Jane Doe", age: 25 },
+    { name: "Bob Smith", age: 40 },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar"
+        onChange={handleChange}
+      />
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Edad</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.filter((item) => item.name.includes(value)).map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.age}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
-}
+};
 
 export default App;
