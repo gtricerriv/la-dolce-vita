5d20287169fc74ca5bc2c003cf82cbd6cd21f6c6
Giancarlo Tricerri <104391091+gtricerriv@users.noreply.github.com>
2023-11-03 12:56:25 -0400

    update table

--- front/src/App.js
+++ front/src/App.js
@@ -1,44 +1,62 @@
-import React, { useState } from "react";
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const App = () => {
-  const [value, setValue] = useState("");
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
-  const data = [
-    { name: "John Doe", age: 30 },
-    { name: "Jane Doe", age: 25 },
-    { name: "Bob Smith", age: 40 },
-  ];
-
+const DenseTable = function() {
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Buscar"
-        onChange={handleChange}
-      />
-      <Table>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Nombre</TableCell>
-            <TableCell>Edad</TableCell>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.filter((item) => item.name.includes(value)).map((item) => (
-            <TableRow key={item.id}>
-              <TableCell>{item.name}</TableCell>
-              <TableCell>{item.age}</TableCell>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-    </div>
+    </TableContainer>
   );
-};
+}
 
-export default App;
+export default DenseTable;
